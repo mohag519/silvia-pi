c5ddd374838436becb1f1c9fd3013d8af23cebe3
xascrimson <dannyhuang1234@hotmail.com>
2021-05-31 00:21:10 +1000

    final

--- silvia-pi.py
+++ silvia-pi.py
@@ -241,7 +241,7 @@ if __name__ == '__main__':
     timeState['TimerOnSu'] = conf.TimerOnSu
     timeState['TimerOffSu'] = conf.TimerOffSu
 
-    timeState['overRide'] = False #conf.overRide
+    pidstate['overRide'] = False #conf.overRide
 
     pidstate['awake'] = timer.timer(timeState)
 
