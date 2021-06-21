589b1b36847fb9e3b4949ebadb49fee24dc9fab6
Xascrimson <35974973+Xascrimson@users.noreply.github.com>
2021-06-21 21:06:05 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -35,7 +35,7 @@ pip install --upgrade -r $BASEDIR/requirements.txt
 if ! grep silvia-pi.py /etc/rc.local; then
   echo "Adding entry to /etc/rc.local"
   cp /etc/rc.local /etc/rc.local.bak
-  cat /etc/rc.local | sed 's|^exit 0$|/root/silvia-pi/silvia-pi.py > /root/silvia-pi/silvia-pi.log 2>\&1 \&\n\nexit 0|g' > /etc/rc.local.new
+  cat /etc/rc.local | sed 's|^exit 0$|sudo python /root/silvia-pi/silvia-pi.py & > /root/silvia-pi/silvia-pi.log 2>\&1 \&\n\nexit 0|g' > /etc/rc.local.new
   mv /etc/rc.local.new /etc/rc.local
   chmod 755 /etc/rc.local
 else
